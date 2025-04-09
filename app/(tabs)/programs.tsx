b28079706fefeac26eafe40fd6540090b179ab9d
George Kollias <george.kollias@booking.com>
2025-04-09 22:39:57 +0200

    Adding firebase config

---
+++ app/(tabs)/programs.tsx
@@ -0,0 +1,3 @@
+export default function ProgramsScreen() {
+    return null; // Replace with your actual component
+  }
--- utils/firebase.ts
+++ utils/firebase.ts
@@ -9,13 +9,13 @@ import { getFunctions } from 'firebase/functions';
 import { getMessaging } from 'firebase/messaging';
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyCcOS2V5iYk7xwbzyNiAQu3dDqNMWsbwjU",
+  authDomain: "life-organizer-gk.firebaseapp.com",
+  projectId: "life-organizer-gk",
+  storageBucket: "life-organizer-gk.firebasestorage.app",
+  messagingSenderId: "674209590187",
+  appId: "1:674209590187:web:8ee303aa0934907873c0ee",
+  measurementId: "G-V8DX06D9BG"
 };
 
 // Initialize Firebase
